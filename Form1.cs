6cf434204d21d0c51ad876e255b30b7d1309af33
Jānis Tulišs <janistuliss3@gmail.com>
2020-04-27 16:43:53 +0300

    16:43

--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,14 @@ namespace BouncingBall
         public Form1()
         {
             InitializeComponent();
+            InitializeApp();
+            
+        }
+
+        private void InitializeApp()
+        {
+            verVelocity = ballStep;
+            horVelocity = ballStep;
             InitializeMainTimer();
         }
 
@@ -30,17 +38,39 @@ namespace BouncingBall
             mainTimer.Tick += new EventHandler(MainTimer_Tick);
             mainTimer.Interval = 20;
             mainTimer.Start();
-        }
+        }        
 
         private void MainTimer_Tick(object sender, EventArgs e)
         {
             MoveBall();
+            BallBorderCollider();
         }
 
         private void MoveBall()
         {
             Ball.Top += verVelocity;
             Ball.Left += horVelocity;
+        }
+
+        private void BallBorderCollider()
+        {
+            if (Ball.Top + Ball.Height > ClientRectangle.Height) //collide with bottom border
+            {
+                verVelocity = -ballStep;
+                
+            }
+            else if (Ball.Top < 0) //collide with top border
+            {
+                verVelocity = +ballStep;
+            }
+            else if (Ball.Left < 0) //collide with left border
+            {
+                horVelocity = +ballStep;
+            }
+            else if(Ball.Left + Ball.Width > ClientRectangle.Width) //collide with right border
+            {
+                horVelocity = -ballStep;
+            }
         }
     }
 }
