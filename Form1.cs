9097056d53d7257facd7e82bcc9cacfb02adad65
Jānis Tulišs <janistuliss3@gmail.com>
2020-04-27 17:19:58 +0300

    17:16

--- Form1.cs
+++ Form1.cs
@@ -29,6 +29,10 @@ namespace BouncingBall
         {
             verVelocity = ballStep;
             horVelocity = ballStep;
+
+            this.KeyDown += new KeyEventHandler(App_KeyDown);
+
+            UpdateBallStepLabel();
             InitializeMainTimer();
         }
 
@@ -56,21 +60,59 @@ namespace BouncingBall
         {
             if (Ball.Top + Ball.Height > ClientRectangle.Height) //collide with bottom border
             {
-                verVelocity = -ballStep;
+                verVelocity *= -1;
+                //verVelocity = -ballStep;
                 
             }
             else if (Ball.Top < 0) //collide with top border
             {
-                verVelocity = +ballStep;
+                verVelocity *= -1;
+                // verVelocity = +ballStep;
             }
             else if (Ball.Left < 0) //collide with left border
             {
-                horVelocity = +ballStep;
+                horVelocity *= -1;
+                //horVelocity = +ballStep;
             }
             else if(Ball.Left + Ball.Width > ClientRectangle.Width) //collide with right border
             {
-                horVelocity = -ballStep;
+                horVelocity *= -1;
+                //horVelocity = -ballStep;
             }
         }
+
+        private void App_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.X)
+            {
+                ballStep += 1;
+                if (ballStep != 0)
+                {
+                    verVelocity = ballStep * (verVelocity/Math.Abs(verVelocity));
+                    horVelocity = ballStep * (horVelocity / Math.Abs(horVelocity));
+                }
+                else
+                {
+                    verVelocity = ballStep;
+                    horVelocity = ballStep;
+                }
+                                
+                UpdateBallStepLabel();
+            }
+            else if(e.KeyCode == Keys.Z)
+            {
+                if(ballStep > 0)
+                {
+                    ballStep -= 1;
+                    UpdateBallStepLabel();
+                }                
+            }                
+        }
+
+        private void UpdateBallStepLabel()
+        {
+            BallStepLabel.Text = "Ball step: " + ballStep;
+        }
+
     }
 }
